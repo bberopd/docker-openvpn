c57e0e7220fc44b27fcaac30f26550129502a3ae
bberopd <60280409+bberopd@users.noreply.github.com>
2020-01-25 08:15:05 +0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,9 +9,9 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-# Allow UDP traffic on port 1194.
-iptables -A INPUT -i eth0 -p udp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
-iptables -A OUTPUT -o eth0 -p udp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
+# Allow TCP traffic on port 1194.
+iptables -A INPUT -i eth0 -p tcp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
 
 # Allow traffic on the TUN interface.
 iptables -A INPUT -i tun0 -j ACCEPT
