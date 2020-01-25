bee20374c2d80ce9529d0988ec283cee7c294948
bberopd <60280409+bberopd@users.noreply.github.com>
2020-01-25 18:27:41 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run --cap-add=NET_ADMIN \
--p 1194:1194/tcp -p 80:8080/tcp \
+-p 1194:1194/tcp -p 8080:8080/tcp \
 -e HOST_ADDR=localhost \
 --name dockovpn --rm \
 alekslitvinenk/openvpn "$@"
